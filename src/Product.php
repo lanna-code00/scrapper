33f1a017701a35c5ea41d9042c30684d7993d115
Mezeobidi <mezeobidi@trav4college.com>
2022-07-08 00:40:02 +0200

    splitted the codes into functions

--- src/Product.php
+++ src/Product.php
@@ -1,8 +1,115 @@
 <?php
 
 namespace App;
+use Symfony\Component\DomCrawler\Crawler;
 
 class Product
 {
+    public static function getAllProducts($document){
+        $allProducts = [];
+
+        if ($document) {
+            foreach ($document as $key => $currentDocument) {
+                $currentDocument->filter("div.flex-wrap div.product")->each(function(Crawler $node, $index) use(&$allProducts, &$getCurentColor){
+                    $allProducts[] = [
+                        "title" => $node->filter("span.product-name")->text(""),
+                        "price" => str_replace("£","",$node->filter("div.text-lg")->text("")),
+                        "image" => str_replace("../",'',"https://www.magpiehq.com/developer-challenge/".$node->filter("img")->attr('src')),
+                        'capacityMB' => Product::getCapacityInMB($node->filter("span.product-capacity")->text("")),
+                        'colour' => Product::getFirstColour($node->filter("div.px-2 span.rounded-full")),
+                        'availabilityText'=> $availabilityText = str_replace("Availability:","", $node->filter("div.text-sm")->text("")),
+                        'isAvailable'=> ( str_contains($availabilityText, "Out of Stock") ) ? false : true ,
+                        'shippingText'=> ($node->filter("div.text-sm")->last()->text("") == "Availability: Out of Stock") ? ""  : $node->filter("div.text-sm")->last()->text(""),
+                        'shippingDate'=> $node->filter("div.text-sm")->last()->text(""),
+                    ];
+                });
+            }
+        }
+        $allProducts = Product::getDuplicateColor($allProducts);
+        return  array_values(array_unique($allProducts,SORT_REGULAR));
+    }
+
+    private static function getCapacityInMB(string $capacity){
+        $capacityMb = "";
+        if (substr($capacity, -2) == "GB") { 
+            $trimmedGb = str_replace(' ', '', $capacity);
+            $parsedGb = (int) substr($trimmedGb, 0, strlen($trimmedGb)-2);
+            $capacityMb = $parsedGb*1024 ."MB";
+        }else{
+            $capacityMb = (string)filter_var($capacity); //getting string from value given
+        }
+
+        return $capacityMb; 
+    }
+
+    private static function getFirstColour($currentNode){
+     
+        $colour = [];
+        $currentNode->each(function(Crawler $node, $index) use(&$colour){
+            $colour[] = $node->filter("span.rounded-full")->attr("data-colour");
+        });
+        return $colour ;
+
+}
+
+    public static function getDate(string $shippingDate){
+        $date = "";
+        
+        if (str_contains($shippingDate , "Jul") || str_contains($shippingDate , "Aug")) { 
+            $month = "";
+            $day = "";
+            $contain_date = substr($shippingDate, -15);
+            if (str_contains($shippingDate , "Jul")) {
+               $month = "07";
+               $arr = explode("Jul", $contain_date , 2);
+               $day = $arr[0];
+            }
+            if(str_contains($shippingDate , "Aug")){
+                $month = "08";
+                $arr = explode("Aug", $contain_date , 2);
+                $day = $arr[0];
+            }
+
+            $date = "2022-". $month ."-". $retVal = (strlen((int) filter_var($day, FILTER_SANITIZE_NUMBER_INT) ) > 1) ?(int) filter_var($day, FILTER_SANITIZE_NUMBER_INT) : "0".(int) filter_var($day, FILTER_SANITIZE_NUMBER_INT);
+        }
+        if (str_contains($shippingDate , "2022-")) { 
+            $date = substr($shippingDate, -10); 
+        }
+        if (str_contains($shippingDate , "tomorrow")) {
+            $tomorrow = strtotime("+1 day");
+
+            //Format the timestamp into a date string
+            $date = date("Y-m-d", $tomorrow);
+        }
+
+        //jul, aug,
+       
+        return $date;
+    }
+
+    private static function getDuplicateColor($products){
+        $new_color_products = [];
+
+        if ($products) {
+            foreach ($products as $products_value) {
+                if($products_value['colour']){
+                    foreach ($products_value['colour'] as $new_products_value) {
+                        $new_color_products[] = [
+                            'title' => $products_value['title'],
+                            'price' => $products_value['price'],
+                            'image' => $products_value['image'],
+                            'capacityMB' => $products_value['capacityMB'],
+                            'colour' => $new_products_value,
+                            'availabilityText'=> $products_value['availabilityText'],
+                            'isAvailable'=> $products_value['isAvailable'],
+                            'shippingText'=> $products_value['shippingText'],
+                            'shippingDate'=>Product::getDate( $products_value['shippingDate']),
+                        ];
+                    }
+                }
+            }
+        }
+        return $new_color_products;
+    }
     
 }
--- src/Scrape.php
+++ src/Scrape.php
@@ -1,74 +1,24 @@
 <?php
+
 namespace App;
 
-require 'C:/projects/learnings/web_scrapping/vendor/autoload.php';
+require 'vendor/autoload.php';
 
 class Scrape
 {
-    private array $products = array();
-    protected $dates;
-    protected $page;
+    private array $products = [];
 
-    public function run($pageNo): void
-    {
-        $document = ScrapeHelper::fetchDocument("https://www.magpiehq.com/developer-challenge/smartphones/?page=$pageNo");
-        $document->filter('#products > p')->each(function ($node, $i) {
-            $this->page = $node->text();
-        });
-        $document->filter('#products > div.flex.flex-wrap.-mx-4 > div > div')->each(function ($node, $i) {
-            $imgs = $node->filter('img')->attr('src');
-            $newImages = preg_replace( "/^\.+|\.+$/", "", $imgs);
-            $images = 'https://www.magpiehq.com/developer-challenge'.$newImages;
-            $capacityGb = $node->filter('h3 > span.product-capacity')->text();
-            $trimmedGb = str_replace(' ', '', $capacityGb);
-            $parsedGb = (int) substr($trimmedGb, 0, strlen($trimmedGb)-2);
-            $price = $node->filter('div.my-8.block.text-center.text-lg')->text();
-            $colors = $node->filter('div > div > div > span.border.border-black.rounded-full.block')->first()->attr('style');
-            $newdata = explode(' ', $colors);
-            $myDate = $node->filter('div.my-4.text-sm.block.text-center')->last()->text();
-            $statusMessage = explode(" ", $myDate);
-            $SliceDate = array_slice($statusMessage, -3, 3);
-            $date = implode(" ", $SliceDate);
-
-            if ((int)$date) {
-                $convertedDate = (string)$date;
-                $newDate = date_create($convertedDate);
-                $this->dates = date_format($newDate,"Y-m-d");
-            }
-
-            $myArr = array(
-                // "pages" => $this->page,  If this any need to add pages to the json uncomment this line of code
-                "imageUrl" => $images,
-                "title" => $node->filter('h3 > span.product-name')->text(),
-                "capacityMb" => $parsedGb*1024 ."MB",
-                "price" => substr($price, 2),
-                "availabilityText" => $node->filter('div.my-4.text-sm.block.text-center')->first()->text(),
-                "isAvailable" => $node->filter('div.my-4.text-sm.block.text-center')->first()->text() == "Availability: Out of Stock" ? false : true,
-                "shipText" => $node->filter('div.my-4.text-sm.block.text-center')->last()->text(),
-                "shipdate" => $this->dates,
-                "color" => $newdata[5],
-            );
-
-            //Saving it inside the file
-
-            if (filesize("output.json") && filesize("output.json") == 0) {
-                $first_record = array($myArr);
-                $data_to_save = $first_record;
-            } else {
-                $old_records = json_decode(file_get_contents("output.json")) ? json_decode(file_get_contents("output.json")) : array();
-
-                array_push($old_records, $myArr);
-                $data_to_save = $old_records;
-            }
-file_put_contents("output.json", json_encode($data_to_save, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT), LOCK_EX);
-           
-    });
-
+    public function run(): void
+    {   
+        $url = "https://www.magpiehq.com/developer-challenge/smartphones/";
+        //getting pagination
+        for ($i=0; $i < 3 ; $i++) { 
+            $document[$i] = ScrapeHelper::fetchDocument($url.'?page='.$i+1);
+        }
+        $this->products = Product::getAllProducts($document); 
+        file_put_contents('output.json', str_replace("\/", "/",json_encode($this->products, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT)));
     }
-
 }
 
 $scrape = new Scrape();
-for ($i=1; $i <= 3; $i++) { 
-    $scrape->run($i);
-}
+$scrape->run();
